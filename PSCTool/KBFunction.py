2a51251308ea8c146e79f0e4d696731ed1f6b784
Swarfte <wcpbenchau@gmail.com>
2021-10-10 23:24:28 +0800

    加入了頭尾字母匹配功能 v0.6.0

--- PSCTool/KBFunction.py
+++ PSCTool/KBFunction.py
@@ -2,44 +2,92 @@ import json
 import pynput.keyboard as pk
 import re
 
-def on_press_text(key):
+
+def on_press_text(key):  # 顯示按下的鍵
     try:
         print(f"按下了 {key.char} 鍵")
-    except AttributeError: # 特殊按鍵的情況
+    except AttributeError:  # 特殊按鍵的情況
         print(f"按下了 {key} 鍵")
 
-def on_release_text(key):
+
+def on_release_text(key):  # 顯示放開的鍵
     print(f"鬆開了 {key} 鍵")
 
-def find_in_head():
-    pass
 
-def find_in_inside(vocabulary,data):
+def find_in_head(vocabulary, data):  # 由頭開始搜索
     sentence = "".join(vocabulary)
-    choice = re.compile(f"{sentence}")
+    print(f"字符 {sentence} 啟用首字母匹配模式")
+    choice = re.compile(f"^{sentence}")
     match_word = []
     for x in range(len(data)):
-        temp = re.search(choice,data[x])
-        #print(str(temp))
-        if str(temp) != "None" :
+        temp = re.search(choice, data[x])
+        if str(temp) != "None":
             match_word.append(data[x])
     return match_word
 
-def open_json(path):
-    with open (path,"r",encoding="utf-8") as f:
+
+def find_in_inside(vocabulary, data):  # 搜索是否包含在內
+    sentence = "".join(vocabulary)
+    print(f"字符 {sentence} 啟用包含字母匹配模式")
+    match_word = []
+    for x in range(len(vocabulary)):
+        temp = []
+        if x == 0:
+            for y in range(len(data)):
+                if vocabulary[x] in data[y]:
+                    temp.append(data[y])
+                    match_word.append(temp)
+        else:
+            for z in range(len(match_word[x - 1])):  # 層層篩選
+                if vocabulary[x] in match_word[x - 1][z]:
+                    temp.append(match_word[x - 1][z])
+                    match_word.append(temp)
+
+    return match_word[len(match_word) - 1]  # 回傳包含全部篩選生字的單詞
+
+
+def find_in_head_and_tail(config, vocabulary):  # 查找頭尾匹配的生字
+    sentence = "".join(vocabulary)
+    print(f"字符 {sentence} 啟用頭尾字母匹配模式")
+    data = get_word_list(config, vocabulary)
+    choice = re.compile(f"^{vocabulary[0]}[a-z]*{vocabulary[1]}$")
+    match_word = []
+    for x in range(len(data)):
+        temp = re.search(choice, data[x])
+        if str(temp) != "None":
+            match_word.append(data[x])
+
+    print(match_word)
+    return match_word
+
+def open_json(path):  # 加載設定檔
+    with open(path, "r", encoding="utf-8") as f:
         data = json.load(f)
     return data
 
-def completion(config,vocabulary):
-    key_match = "".join(vocabulary)
-    print(f"檢測數據庫中包含 {key_match} 的生字")
-    target = "WordBank_path"#指定讀取的數據庫
-    vocabulary_type = "english_word" #指定讀取的生字
-    data = open_json(open_json(config)[target])[vocabulary_type] #*獲取對應數據庫的全部英文生字
-    #key_match = [x for x in data if str(last_key).replace("'","") in x[:1]]#去除多餘的單引號並匹配第一個生字
-    print(find_in_inside(vocabulary, data))
 
-def save_words_setup(key_input):
+def key_word_match(config, vocabulary, begin):  # 多生字匹配模式
+    data = get_word_list(config, vocabulary)
+    if begin:  # 判斷是首字母匹配還是包含匹配模式
+        match_word = find_in_head(vocabulary, data)
+    else:
+        match_word = find_in_inside(vocabulary, data)
+
+    print(match_word)
+
+    return match_word
+
+
+def get_word_list(config, vocabulary):  # 獲取數據庫的生字
+    key_match = "".join(vocabulary)
+    target = "WordBank_path"  # 指定讀取的數據庫
+    vocabulary_type = "english_word"  # 指定讀取的生字
+    data = open_json(open_json(config)[target])[vocabulary_type]  # *獲取對應數據庫的全部英文生字
+
+    return data
+
+
+def save_words_setup(key_input):  # 儲存生字功能
     if key_input:
         print("關閉 儲存生字功能")
         return False
@@ -47,18 +95,23 @@ def save_words_setup(key_input):
         print("開啟 儲存生字功能")
         return True
 
-def save_words(key,vocabulary):
-    if key != pk.Key.ctrl_l and key != pk.Key.caps_lock:  # 無視左ctrl的操作和檢測操作人
-        vocabulary.append(str(key).replace("'", ""))  # 去除多餘的單引號
 
-def clean_save_word(vocabulary):
+def save_words(key, vocabulary):  # 判斷生字
+    if key != pk.Key.ctrl_l and key != pk.Key.caps_lock:  # 無視左ctrl的開關操作和檢測生字操作
+        if key != pk.Key.shift_l and key != pk.Key.shift_r:  # 無視左右shirt的搜索功能
+            vocabulary.append(str(key).replace("'", ""))  # 去除多餘的單引號
+
+
+def clean_save_word(vocabulary):  # 清空保留的生字
     print('清空儲存的生字')
     vocabulary = []
     return vocabulary
 
-def exitListen():
+
+def exit_listen():  # 退出監聽
     print("退出全局監聽")
     return False  # 回傳false停止監聽
 
-def current_words(vocabulary):
+
+def current_words(vocabulary):  # 顯示當前的生字
     print(vocabulary)
--- PSCTool/KBListener.py
+++ PSCTool/KBListener.py
@@ -1,52 +1,58 @@
-
 import pynput.keyboard as pk
 import PSCTool.KBFunction as PF
 
-#全局變量
+# 鍵盤監聽全局變量
 config = None
 vocabulary = []
 key_input = False
 
-def load_json(setting): #讀取設定檔用
+
+def load_json(setting):  # 讀取設定檔用
     global config
     config = setting
 
-def key_down(key):#按下按鍵時執行
-    PF.on_press_text(key) #輸出文本
 
-def key_up(key):#鬆開按鍵時執行
-    global config #設定檔
-    global vocabulary #待查詢的生字
-    global key_input # 儲存生字的開關
+def key_down(key):  # 按下按鍵時執行
+    PF.on_press_text(key)  # 輸出文本
 
-    PF.on_release_text(key) #輸出文本
 
-    #設置退出監聽優先級最高
-    if key == pk.Key.esc: #退出監聽
-        return PF.exitListen()
+def key_up(key):  # 鬆開按鍵時執行
+    global config  # 設定檔
+    global vocabulary  # 待查詢的生字
+    global key_input  # 儲存生字的開關
 
-    if key == pk.Key.ctrl_l: #*啟動/關閉儲存生字的功能
+    PF.on_release_text(key)  # 輸出文本
+
+    # 設置退出監聽優先級最高
+    if key == pk.Key.esc:  # 退出監聽
+        return PF.exit_listen()
+
+    if key == pk.Key.ctrl_l:  # *啟動/關閉儲存生字的功能
         key_input = PF.save_words_setup(key_input)
 
-    if key == pk.Key.caps_lock:#顯示目前的生字
+    if key == pk.Key.alt_l:  # 顯示目前的生字
         PF.current_words(vocabulary)
 
-    if key_input: #啟用時儲存生字
-        PF.save_words(key,vocabulary)
+    if key_input:  # 啟用時儲存生字
+        PF.save_words(key, vocabulary)
 
-
-    if key == pk.Key.alt_l: #*清空儲存的生字
+    if key == pk.Key.alt_r:  # *清空儲存的生字
         vocabulary = PF.clean_save_word(vocabulary)
 
-    if len(vocabulary) != 0:
-        if key == pk.Key.shift_l: #啟動搜索功能
-            PF.completion(config, vocabulary)
+    if key == pk.Key.shift_l:  # 按生字搜索功能
+        if len(vocabulary) > 0:
+            PF.key_word_match(config, vocabulary, key_input)  # True為啟動首字母匹配功能,False為啟動包含搜索
 
-class KBListener(): #用於監聽鍵盤線程
-    def __init__(self,config):
+    if key == pk.Key.shift_r:  # 啟動頭尾字母匹配模式
+        if len(vocabulary) == 2:
+            PF.find_in_head_and_tail(config, vocabulary)
+
+
+class KBListener:  # 用於監聽鍵盤線程
+    def __init__(self, config):
         super(KBListener, self).__init__()
         load_json(config)
 
-    def start(self): #啟動線程
+    def start(self):  # 啟動線程
         with pk.Listener(on_press=key_down, on_release=key_up) as listener:
             listener.join()
